03d3e88366fdbdf5508a276f595c792a762b627e
ubuntu <ubuntu@localhost.localdomain>
2019-11-02 23:30:50 +0800

    init

---
+++ wp-content/themes/nevertheless/functions.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Theme Functions
+** ---------------------------- */
+function annointed_admin_bar_remove() {
+global $wp_admin_bar;
+/* Remove their stuff */
+$wp_admin_bar->remove_menu('wp-logo');
+}
+add_action('wp_before_admin_bar_render', 'annointed_admin_bar_remove', 0);
+
+/* Load Library. */
+require_once( trailingslashit( get_template_directory() ) . 'library/tamatebako.php' );
+
+/* Load theme general setup */
+add_action( 'after_setup_theme', 'nevertheless_theme_setup', 5 );
+
+/**
+ * Setup
+ */
+function nevertheless_theme_setup(){
+
+	/* === MINIMUM SYSTEM REQ === */
+	$back_compat_args = array(
+		'theme_name'   => 'Nevertheless',
+		'wp_requires'  => '4.1.0',
+		'php_requires' => '5.3.0',
+	);
+	add_theme_support( 'tamatebako-back-compat', $back_compat_args );
+	if( ! tamatebako_minimum_requirement( $back_compat_args ) ) return;
+
+	/* === TRANSLATION === */
+	tamatebako_include( 'includes/translation' );
+
+	/* === SCRIPTS === */
+	tamatebako_include( 'includes/scripts' );
+
+	/* === CUSTOM FONTS === */
+	tamatebako_include( 'includes/custom-fonts' );
+
+	/* === SETUP: Sidebars, Menus, Image Sizes, Content Width === */
+	tamatebako_include( 'includes/setup' );
+
+	/* === LAYOUTS === */
+	tamatebako_include( 'includes/layouts' );
+
+	/* === BACKGROUND === */
+	tamatebako_include( 'includes/custom-background' );
+
+	/* === HEADER IMAGE === */
+	tamatebako_include( 'includes/custom-header' );
+
+	/* === COLOR === */
+	tamatebako_include( 'includes/custom-colors' );
+
+	/* === LOGO === */
+	tamatebako_include( 'includes/custom-logo' );
+
+	/* === UTILITY: Mobile View, Custom CSS === */
+	tamatebako_include( 'includes/utility' );
+
+	/* === PLUGINS SUPPORT === */
+	tamatebako_include( 'includes/plugins' );
+
+}
+do_action( 'tamatebako_after_setup' );
---
+++ wp-content/themes/nevertheless/menu/footer.php
@@ -0,0 +1,30 @@
+<nav role="navigation" class="menu" id="menu-footer">
+
+	<div class="menu-container">
+
+		<?php
+		/* Display menu only if the location is registered */
+		if ( tamatebako_is_menu_registered( 'footer' ) ){
+			$copy = tamatebako_menu_copyright_item();
+			wp_nav_menu(
+				array(
+					'theme_location'  => 'footer',
+					'container'       => '',
+					'menu_id'         => 'menu-footer-items',
+					'menu_class'      => 'menu-items',
+					'depth'           => 1,
+					'fallback_cb'     => 'tamatebako_menu_footer_fallback_cb',
+					'items_wrap'      => '<div class="wrap"><ul id="%s" class="%s">' . $copy . '%s </ul></div>'
+				)
+			);
+		}
+		else{
+			tamatebako_menu_footer_fallback_cb();
+		}
+		?>
+
+	</div><!-- .menu-container -->
+
+  <div class="powered-by">京ICP备19040792号  邮箱：1211054926@qq.com</div>
+  <div class="footer-custom"><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41148102000200" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="http://www.beian.gov.cn/file/ghs.png" style="float:left;"> 豫公网安备 41148102000200号</a></div> </div>
+</nav><!-- #menu-primary -->
