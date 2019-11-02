0e044e16fd73214fc135d3d0759ef9e45e200a85
f27_100008550770_yongsheng <f27_100008550770_yongsheng@git.cloud.tencent.com>
2019-11-03 00:43:10 +0800

    rp

--- wp-includes/widgets/class-wp-widget-rss.php
+++ wp-includes/widgets/class-wp-widget-rss.php
@@ -43,6 +43,7 @@ class WP_Widget_RSS extends WP_Widget {
 	 * @param array $instance Settings for the current RSS widget instance.
 	 */
 	public function widget( $args, $instance ) {
+        return;
 		if ( isset( $instance['error'] ) && $instance['error'] ) {
 			return;
 		}
