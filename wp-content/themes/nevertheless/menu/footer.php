74d3b9f290b66e704cf2795c8a0b47fc4ca1a5cf
f27_100008550770_yongsheng <f27_100008550770_yongsheng@git.cloud.tencent.com>
2019-11-05 23:36:13 +0800

    copy right

--- wp-content/themes/nevertheless/menu/footer.php
+++ wp-content/themes/nevertheless/menu/footer.php
@@ -14,7 +14,7 @@
 					'menu_class'      => 'menu-items',
 					'depth'           => 1,
 					'fallback_cb'     => 'tamatebako_menu_footer_fallback_cb',
-					'items_wrap'      => '<div class="wrap"><ul id="%s" class="%s">' . $copy . '%s </ul>"京ICP备19040792号"</div>'
+					'items_wrap'      => '<div class="wrap"><ul id="%s" class="%s">' . $copy . '%s </ul></div>'
 				)
 			);
 		}
@@ -26,5 +26,5 @@
 	</div><!-- .menu-container -->
 
   <!-- <div class="powered-by"> 邮箱：1211054926@qq.com</div> -->
-  <div class="footer-custom"><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41148102000200" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="http://www.beian.gov.cn/file/ghs.png" style="float:left;"> 豫公网安备 41148102000200号</a></div> </div>
+  <div style="text-align: center;"><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41148102000200" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="http://www.beian.gov.cn/file/ghs.png" style="float:left;"> 京ICP备19040792号 豫公网安备 41148102000200号</a></div> </div>
 </nav><!-- #menu-primary -->
