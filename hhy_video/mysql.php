f186a3fc484f69810af08aa4e633b23cc7538c81
lichanghong <>
2020-02-01 22:17:36 +0800

    model

--- hhy_video/mysql.php
+++ hhy_video/mysql.php
@@ -16,12 +16,31 @@ if (!$select_db) {
     die("could not connect to the db:\n" .  $mysqli->error);
 }
 
+//新建数组
+$arr=array();
+//2.转换为对象，处理数据
+class Vip_Video{
+    public $video_title;
+    public $cover_img;
+    public $video_desc;
+    public $video_url;
+    public $submission_date;
+}
+
 $sql = "select * from hhy_vip_video;";
 if ($result = $mysqli->query($sql)) {
     /* fetch associative array */
     while ($row = $result->fetch_assoc()) {
-        printf ("%s (%s)\n", $row["video_title"], $row["cover_img"]);
+        $vv=new Vip_Video();
+        $vv->video_title=$row["video_title"];
+        $vv->cover_img=$row["cover_img"];
+        $vv->video_desc=$row["video_desc"];
+        $vv->video_url=$row["video_url"];
+        $vv->submission_date=$row["submission_date"];
+        $arr[]=$vv;
     }
+    echo json_encode(array("state"=>"success",'vip_videos'=>$arr));
+
     /* free result set */
     $result->free();
 }
