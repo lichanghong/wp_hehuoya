d9b2d27fe4ea893fde868c3dd3bd6d2506280a65
lichanghong <>
2020-02-02 21:23:40 +0800

    model

--- hhy_video/hhy_video_player.php
+++ hhy_video/hhy_video_player.php
@@ -2,6 +2,10 @@
 <html lang="zh-CN">
 <head>
  <title>合伙呀视频</title>
+
+ <style type="text/css">
+body,html {margin:0; height:100%;}
+</style>
 </head>
 <body>
  
@@ -12,9 +16,7 @@ $target=$parse.$url;
     echo '<div style="width:100%;height:100%;">';
     echo '<iframe id="WANG" scrolling="no" allowfullscreen="true" allowtransparency="true" frameborder="0"';
     echo "src=\"".$target."\"";
-    echo 'width="100%" height="100%"></iframe>';
-echo $target;
-   
+    echo 'width="100%" height="100%"></iframe>';   
 ?>
         
 </body>
