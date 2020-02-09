92979b1c3e75036ac965699edf1852a133d35594
lich7 <lich7@ziroom.com>
2020-02-09 14:12:38 +0800

    init

--- wp-content/themes/arowana/templates/template-homepage.php
+++ wp-content/themes/arowana/templates/template-homepage.php
@@ -5,7 +5,7 @@ Template Name: Default homepage
 ?>
 <?php 
 	get_header();
-	do_action( 'startkit_sections', false );
+	do_action( 'startkit_sections', true );
 	get_template_part('sections/arowana','blog');
 	get_footer();
 ?>
