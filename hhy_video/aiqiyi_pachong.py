478e411111c417db3c15970c6a157cce74bd361c
ubuntu <ubuntu@localhost.localdomain>
2020-02-09 12:28:31 +0800

    arowana

--- hhy_video/aiqiyi_pachong.py
+++ hhy_video/aiqiyi_pachong.py
@@ -22,25 +22,29 @@ http = urllib3.PoolManager()
 url = "https://www.iqiyi.com/dianying_new/i_list_zuixinshangxian.html"
 
 html_content = http.request('GET', url,).data.decode("utf-8","ignore")
-soup_ccontent = bs4(html_content, 'html.parser')
+soup_content = bs4(html_content, 'html.parser')
 
 
-ul_content = soup_ccontent.find("ul", class_="site-piclist")
+ul_content = soup_content.find("ul", class_="site-piclist")
 li_content = ul_content.find_all("li")
-# filename = 'video.sql'
 
 import pymysql
 db = pymysql.connect("localhost","root","lchcl0655","hhy_vip_video_db")
 cursor = db.cursor()
 
+cursor.execute("select video_title from hhy_vip_video2")
+data= cursor.fetchall()
+
+
 for li in li_content:
     img="https:"+li.img['src'];
     desc=li.find('p', class_="site-piclist_info_describe").string
-    sql = "INSERT INTO hhy_vip_video(video_title,cover_img,video_desc,video_url)values ('"+li.a['title']+"','"+img+"','"+desc+"','"+li.a['href']+"');"
+    movie_name =  li.a['title']
+    if (movie_name,) in data:
+        continue
+    sql = "INSERT INTO hhy_vip_video2(video_title,cover_img,video_desc,video_url)values ('"+movie_name+"','"+img+"','"+desc+"','"+li.a['href']+"');"
     cursor.execute(sql)
     db.commit()
-    
-db.close()
- 
 
- 
+db.close()
+
