2b3bac46199ccf6357f798566185181ee58597a3
lich7 <lich7@ziroom.com>
2020-02-09 12:49:18 +0800

    init

--- wp-content/themes/arowana/header.php
+++ wp-content/themes/arowana/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
+			<script data-ad-client="ca-pub-6387872276467622" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
